4ba2fc1d6520d3c7cb50c1795c04e18b679d89d1
Nutthakit Anansuksakul <ing118199@gmail.com>
2023-05-15 15:18:01 +0700

    done

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { Button ,Stack} from '@mui/material'
+// import { Delete, Send, Photo } from "@mui/icons-material"
+// import AppHeader from './components/AppHeader';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from './layouts/AuthLayout'
+import Login from "./pages/Login";
+import BackendLayout from "./layouts/BackendLayout";
+import Dashboard from './pages/Dashboard';
+import Product from './pages/Product';
+import Setting from './pages/Setting';
+import Report from './pages/Report';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+
+// function App() {
+
+  // return (
+  //   <>
+  //   <h1>MUI Button</h1>
+  //   <Stack spacing = {2} direction='row'>
+  //     <Button variant= 'text'>Text</Button>
+  //     <Button variant='contained'>contained</Button>
+  //     <Button variant='outlined'>Textoutlined</Button>
+  //   </Stack>
+  //   <Stack spacing={2} direct="row">
+  //     <Button variant='text' startIcon = {<Send />}> Start </Button>
+  //     <Button variant='contained' startIcon = {<Delete />}>Delete </Button>
+  //     <Button variant='outlined' starIcon= { <Photo />}>Photo</Button>
+  //   </Stack>
+      
+  //   </>
+  // )
+
+  function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AuthLayout />}>
+            <Route path="/" element={<Login />} />           
+          </Route>
+
+          <Route element={<BackendLayout />}>
+            <Route path="/backend/dashboard" element={<h1>Dashboard</h1>} />
+            <Route path="/backend/product" element={<Product />} />
+            <Route path="/backend/report" element={<Report />} />
+            <Route path="/backend/setting" element={<Setting />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+  
 
 export default App
---
+++ reactapp/src/pages/Login.tsx
@@ -0,0 +1,45 @@
+import { TextField, Button } from "@mui/material"
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  // useForm hook
+  const { register, handleSubmit, formState:{ errors }} = useForm()
+
+  // onSubmit function
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+  return (
+    <>
+        <h1>Login</h1>
+        <form onSubmit = {handleSubmit(onSubmit)}>
+          <div>
+            <TextField label="Username"
+              type="text"
+              variant="outlined"
+              {...register("username", { required: true, minLength:5 })}
+              error={errors.usename ? true : false}
+              helperText = {errors.username ? "Username is required" : ""}
+            />
+          </div>
+
+          <div>
+            <TextField label="Password"
+              type="password"
+              variant="outlined"
+              {...register("password", { required: true })}
+              error={errors.password ? true : false}
+              helperText = {errors.password ? "password is required" : ""}
+            />
+          </div>
+          <div>
+            <Button variant="contained" color="primary" type="submit">
+              Login
+            </Button>
+          </div>
+        </form>
+    </>
+  )
+}
+
+export default Login
